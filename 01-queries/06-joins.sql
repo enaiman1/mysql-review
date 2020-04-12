0d79b6ee35ba4f894ef0ccd0a0df62690512abda
Ericcentrik <enaiman1@gmail.com>
2020-04-12 13:56:20 -0400

    updated join solution

--- 01-queries/06-joins.sql
+++ 01-queries/06-joins.sql
@@ -4,20 +4,20 @@
 -- inner join ( our result only includes rows that match our ON condition.)
 
 -- line 1 - select columns from both tables
-
+SELECT 
 -- add a column called 'order_id' from the 'orders' table
-
+orders.order_id,
 --  add a column called 'customer_name' from the 'customers' table
-
+customers.customer_name
 
 -- line 2 specifies the first table 'orders' (referred in the docs as table one)
+FROM orders
 
-
--- line 3 specifies the second table 'customer'  (referred in the docs as table two)
-
+-- line 3 specifies the second table 'customers'  and what kind of join  (referred in the docs as table two)
+JOIN customers
 
 -- line 4 uses ON cluase to tells us how to combine the two tables
-
+ON orders.customer_id = customers.customer_id;
 
 
 -- USING the customers table and subscriptions table create a left join to join them together
@@ -25,14 +25,14 @@
 --   left joins (combine two tables and keep some of the un-matched rows)
 
 -- line 1 - select columns from both tables
-
+SELECT
 -- add a column called 'customer name' from the 'orders' table
-
+customers.customer_name AS subscriber,
 --  add a column called 'desciption' from the 'customers' table
-
+subscriptions.description AS GENRE
 -- line 2 specifies the first table 'customer' (referred in the docs as table one)
-
--- line 3 specifies the second table 'subscriptions'  (referred in the docs as table two)
-
+From customers
+-- line 3 specifies the second table 'subscriptions' and what kind of join (referred in the docs as table two)
+LEFT JOIN subscriptions
 -- line 4 uses ON cluase to tells us how to combine the two tables
-
+ON customer_id = subscription_id
